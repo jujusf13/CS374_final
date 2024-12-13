fc20f1fc297ef9ab8585ce3a6df7757cc1af95da
ptormey <144549652+ptormey@users.noreply.github.com>
2024-12-13 09:30:01 -0500

    Update main.py

--- main.py
+++ main.py
@@ -3,16 +3,16 @@ from todo import ToDoList, parse_command
 #Pat Tormey --> main file
 def main():
     todo_list = ToDoList()
-    print("Welcome to the To-Do List Command Language!")
+    print("Your to do list manager!!")
     print("Enter commands like: ADD TASK \"Finish project\" PRIORITY HIGH")
     print("Type EXIT to quit.")
     while True:
         command = input("> ")
-        if command.strip().upper() == "EXIT":
+        if command.strip().upper() == "exit":
             print("Goodbye!")
             break
-        response = parse_command(command, todo_list)
-        print(response)
+        output = parse_command(command, todo_list)
+        print(output)
 
 if __name__ == "__main__":
     main()
